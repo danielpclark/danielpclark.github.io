cb66fb11854c11c59e2af384ae69cb058539aafd
Daniel P. Clark <6ftdan@gmail.com>
2017-06-22 12:46:31 -0400

    Add base_custom documentation

---
+++ base_custom/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["BaseCustom","The BaseCustom struct holds the information to perform number conversions via the `gen` and `decimal` methods."]]});
---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["base_custom"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"base_custom/struct.BaseCustom.html\" title=\"struct base_custom::BaseCustom\">BaseCustom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.char.html\">char</a>&gt;","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"base_custom/struct.BaseCustom.html\" title=\"struct base_custom::BaseCustom\">BaseCustom</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/collections/string/struct.String.html\" title=\"struct collections::string::String\">String</a>&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
