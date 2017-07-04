d5631c85d8fbe9565439e889925987505e90cd1d
Daniel P. Clark <6ftdan@gmail.com>
2017-07-04 16:20:46 -0400

    implement partial equality for base_custom

---
+++ implementors/core/cmp/trait.PartialEq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["base_custom"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"base_custom/struct.BaseCustom.html\" title=\"struct base_custom::BaseCustom\">BaseCustom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.char.html\">char</a>&gt;","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"base_custom/struct.BaseCustom.html\" title=\"struct base_custom::BaseCustom\">BaseCustom</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/collections/string/struct.String.html\" title=\"struct collections::string::String\">String</a>&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
--- implementors/core/fmt/trait.Debug.js
+++ implementors/core/fmt/trait.Debug.js
@@ -1,6 +1,5 @@
 (function() {var implementors = {};
 implementors["base_custom"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"base_custom/struct.BaseCustom.html\" title=\"struct base_custom::BaseCustom\">BaseCustom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.char.html\">char</a>&gt;","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"base_custom/struct.BaseCustom.html\" title=\"struct base_custom::BaseCustom\">BaseCustom</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/collections/string/struct.String.html\" title=\"struct collections::string::String\">String</a>&gt;",];
-implementors["digits"] = ["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"digits/struct.Digits.html\" title=\"struct digits::Digits\">Digits</a>&lt;'a&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
