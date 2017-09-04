9c2b238456b95b8b75a0a9cc559393a74b559330
Daniel P. Clark <6ftdan@gmail.com>
2017-09-04 04:16:05 -0400

    digits 0.2.3

---
+++ implementors/core/ops/bit/trait.BitXorAssign.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["digits"] = ["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitXorAssign.html\" title=\"trait core::ops::bit::BitXorAssign\">BitXorAssign</a> for <a class=\"struct\" href=\"digits/struct.Digits.html\" title=\"struct digits::Digits\">Digits</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
