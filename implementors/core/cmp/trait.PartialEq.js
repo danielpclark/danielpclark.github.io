a2a567fd5c37423f2574338874888fab2badb5b7
Daniel P. Clark <6ftdan@gmail.com>
2017-10-01 04:09:33 -0400

    digits 0.3.2

--- implementors/core/cmp/trait.PartialEq.js
+++ implementors/core/cmp/trait.PartialEq.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors["base_custom"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"base_custom/struct.BaseCustom.html\" title=\"struct base_custom::BaseCustom\">BaseCustom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.char.html\">char</a>&gt;","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"base_custom/struct.BaseCustom.html\" title=\"struct base_custom::BaseCustom\">BaseCustom</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/string/struct.String.html\" title=\"struct alloc::string::String\">String</a>&gt;",];
+implementors["digits"] = ["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"digits/struct.Digits.html\" title=\"struct digits::Digits\">Digits</a>&lt;'a&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
