510cb1eeb4d6d27d0e6785467e76ea762c4f2bde
Daniel P. Clark <6ftdan@gmail.com>
2025-02-19 20:15:43 -0500

    Switch to host resume

--- base_custom/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"struct":[["BaseCustom","The BaseCustom struct holds the information to perform number conversions via the `gen` and `decimal` methods."]]});
--- digits/radices/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"fn":[["binary_base","Binary implementation of `BaseCustom`"],["decimal_base","Decimal implementation of `BaseCustom`"],["hex_base","Hexadecimal implementation of `BaseCustom`"],["hexl_base","Lowercase hexadecimal implementation of `BaseCustom`"],["octal_base","Octal implementation of `BaseCustom`"]]});
